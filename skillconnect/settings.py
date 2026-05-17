7349f27d3b3eaa679700299d85039987b76d675d
sweetypappachan <sweetypappachan@gmail.com>
2026-05-17 14:44:52 +0530

    fix gmail smtp settings

--- skillconnect/settings.py
+++ skillconnect/settings.py
@@ -96,5 +96,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'sweetypappachan@gmail.com'
-EMAIL_HOST_PASSWORD = 'orfiwxrsqjcmlkmm'  # no spaces
+EMAIL_HOST_PASSWORD = 'orfiwxrsqjcmlkmm'
 DEFAULT_FROM_EMAIL = 'SkillConnect <sweetypappachan@gmail.com>'
